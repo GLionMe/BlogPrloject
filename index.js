22b6eeca57e8b0491dacc0c2c48881784491ec04
GAURAV KUMAR <gaurav9142@hotmail.com>
2021-11-09 18:31:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = new express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.listen(4000, () => {
+    console.log('App Listening on port 4000');
+});
+
+app.get('/',(req, res) => {
+    res.render('index');
+});
+
+app.get('/about',(req, res) => {
+    res.render('about');
+});
+
+app.get('/post',(req, res) => {
+    res.render('post');
+});
+
+app.get('/contact',(req, res) => {
+    res.render('contact');
+});
