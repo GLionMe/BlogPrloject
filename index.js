82f165cc091e2c9cd1bdcde257263b68a17cfd2b
GAURAV KUMAR <gaurav9142@hotmail.com>
2021-11-28 15:19:10 +0530

    uses express-fileupload to upload images to the server

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-//body parser has been depricated to use with express. Express natively implements the
-//funtionalities of body-parser middleware
+const BlogPost = require('./model/BlogPost');
+const fileUpload = require('express-fileupload');
 
-//connecto to the mongodb database
+//connect to mongodb database
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
 const app = new express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(fileUpload());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -21,16 +21,21 @@ app.listen(4000, () => {
     console.log('App Listening on port 4000');
 });
 
-app.get('/',(req, res) => {
-    res.render('index');
+app.get('/',async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    res.render('index', { blogposts });
 });
 
 app.get('/about',(req, res) => {
     res.render('about');
 });
 
-app.get('/post',(req, res) => {
-    res.render('post');
+app.get('/post/:id',async (req, res) => {
+    console.log(req.params.id);
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post', {
+        blogpost
+    });
 });
 
 app.get('/contact',(req, res) => {
@@ -42,6 +47,12 @@ app.get('/posts/new', (req, res) => {
 });
 
 app.post('/posts/store', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, 'public/assets/img', image.name), async (error) => {
+        await BlogPost.create({
+            ...req.body,
+            image: '/assets/img/' + image.name
+        });
+        res.redirect('/');
+    });
 });
